10e6fb9ed13c1d58f8b5b00ab9f84e818d0ea0be
SocialWorkProgrammer <144265629+SocialWorkProgrammer@users.noreply.github.com>
2024-03-22 12:05:40 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 3784 ms, Memory: 34360 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,3 +1,6 @@
+# import sys
+# sys.stdin = open('input.txt','r')
+
 N = int(input()) # 카드 묶음 개수
 arr = [int(input()) for _ in range(N)]
 # 히든테케가 정렬되어 있을 것이라는 보장이 읎다.
@@ -18,3 +21,13 @@ while arr:
         b = heappop(arr)
         heappush(arr, a + b)
         card_sum += a + b
+
+# 틀린 방식
+# 사유 : 1 1 1 1일 경우 하나씩 더하는 것이 아니라 두개씩 나눠 더하면 더 적은 횟수로 끝남
+# sort후 카드를 하나씩 더함
+# card_sum = 0
+# for i in range(1, N):
+#     card_sum += arr[i] * (N - i)
+# card_sum += arr[0] *(N-1)
+#
+# print(card_sum)
