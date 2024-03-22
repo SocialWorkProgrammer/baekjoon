cfc519729ea529f2c5e5756f60d5a780a8dd0d5f
SocialWorkProgrammer <144265629+SocialWorkProgrammer@users.noreply.github.com>
2024-03-22 16:27:04 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -1,10 +1,18 @@
 ans = ''
-def recur(n):
-    global ans
-    if n == N:
-        return ans + 'int'
-    ans += 'long '
-    return recur(n+4)
+## 재귀
+# def recur(n):
+#     global ans
+#     if n == N:
+#         return ans + 'int'
+#     ans += 'long '
+#     return recur(n+4)
+#
+# N = int(input())
+# print(recur(0))
 
+# 정석
 N = int(input())
-print(recur(0))
+for i in range(N//4):
+    ans += 'long '
+ans += 'int'
+print(ans)
