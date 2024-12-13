4bfc24b82686969743bbbb016cf4c0fb087e6132
Ho Min <144265629+SocialWorkProgrammer@users.noreply.github.com>
2024-12-13 16:33:01 +0900

    [Bronze II] Title: 분해합, Time: 1480 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -4,12 +4,9 @@ ans = 0
 
 for i in range(N+1):
     arr = []
-    if i == N:
+    for j in str(i):
+        arr.append(int(j))
+    if sum(arr) + i == N:
+        ans = i
         break
-    else:
-        for j in str(i):
-            arr.append(int(j))
-        if sum(arr) + i == N:
-            ans = i
-            break
 print(ans)
