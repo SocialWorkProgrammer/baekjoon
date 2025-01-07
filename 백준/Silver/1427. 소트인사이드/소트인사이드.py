10d580de533d37aa1842b2269ed6745409ded740
Ho Min <144265629+SocialWorkProgrammer@users.noreply.github.com>
2025-01-07 10:36:38 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -0,0 +1,14 @@
+import sys
+
+N = list(map(int, input()))
+cntlst = [0] * 11
+ans = ''
+
+for i in N:
+    cntlst[i] += 1
+
+for j in range(10):
+    for k in range(cntlst[j]):
+        ans += str(j)
+
+print(ans[::-1])
