1a507dce4afebaada43b4018fc302c703179488f
Ho Min <144265629+SocialWorkProgrammer@users.noreply.github.com>
2025-01-11 10:32:40 +0900

    [Silver IV] Title: 문자열 집합, Time: 600 ms, Memory: 43232 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,9 +2,12 @@ N, M = map(int, input().split())
 
 setlst = {input() for i in range(N)}
 wordlst = [input() for tc in range(M)]
-cnt = 0
-for word in wordlst:
-    if word in setlst:
-       cnt += 1
 
-print(cnt)
+# 풀어쓴 코드
+# cnt = 0
+# for word in wordlst:
+#     if word in setlst:
+#        cnt += 1
+
+# 컴프리헨션
+print(sum(1 for word in wordlst if word in setlst))
