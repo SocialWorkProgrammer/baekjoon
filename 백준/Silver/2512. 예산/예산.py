c4188535a0abc70b467decc707d19e0c8f137873
Ho Min <144265629+SocialWorkProgrammer@users.noreply.github.com>
2025-04-02 19:01:20 +0900

    [Silver II] Title: 예산, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,24 +1,20 @@
 N = int(input())
-lst = list(map(int,input().split()))
+lst = sorted([int(x) for x in input().split()])
 M = int(input())
 
-# s = min(lst) # 이렇게 할 경우 입력값이 모두 같으면 고장나게 됨 ex : 5 5 5 5 5, budget = 10
 s = 1
-e = max(lst)
-
-ans = 0
+e = lst[-1]
 while s <= e:
     mid = s + (e-s)//2
-    budgetsum = 0
+    sums = 0
     for i in lst:
         if i <= mid:
-            budgetsum += i
+            sums += i
         else:
-            budgetsum += mid
-    if budgetsum <= M:
-        s = mid + 1
+            sums += mid
+    if sums <= M:
         ans = mid
+        s = mid + 1
     else:
         e = mid - 1
-
 print(ans)
