e9a0aeb3432506149b2f1675e50f8c736f435612
Ho Min <144265629+SocialWorkProgrammer@users.noreply.github.com>
2025-05-07 17:42:12 +0900

    [Gold V] Title: 용액, Time: 124 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,17 +1,20 @@
 N = int(input())
 lst = list(map(int, input().split()))
 
-s, e = 0, N-1
-final = [lst[s], lst[e]]
+s = 0
+e = N-1
+inf = float("inf")
+minlst = [inf, inf]
 while s < e:
-    left, right = lst[s], lst[e]
-    mid = left + right
-    if abs(mid) <= abs(sum(final)):
-        final = [left, right]
-        if mid == 0:
-            break
-    if mid < 0:
-        s += 1
-    elif mid > 0:
-        e -= 1
-print(*final)
+    mid = lst[s] + lst[e]
+    if mid == 0:
+        minlst = [lst[s], lst[e]]
+        break
+    else:
+        if abs(mid) < abs(sum(minlst)):
+            minlst = [lst[s], lst[e]]
+        if mid < 0:
+            s += 1
+        else:
+            e -= 1
+print(*minlst)
